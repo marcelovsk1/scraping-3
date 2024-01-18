ab8ae1c8933deed4636bfa50da11dda522d66c85
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-18 14:50:09 -0300

    working on

--- ticketmaster.py
+++ ticketmaster.py
@@ -20,10 +20,12 @@ for event in events:
     event_name = event.find('span', class_='sc-fyofxi-5 gJmuwa').text
     event_month = event.find('div', class_='sc-1evs0j0-1 gwWuEQ').text.strip()
     event_day = event.find('div', class_='sc-1evs0j0-2 ftHsmv').text.strip()
+    event_location = event.find('span', class_='sc-fyofxi-7 PpnvD').text.strip()
 
     print(f"{event_name}")
     print(f"{event_month}")
     print(f"{event_day}")
-    # print(event.text)
+    print(f"{event_location}")
+
 
 driver.quit()
