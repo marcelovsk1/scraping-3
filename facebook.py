ba4ee7702cb4f792c8f6333999343fa6052aafe4
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-18 14:24:17 -0300

    working on

--- facebook.py
+++ facebook.py
@@ -1,3 +1,4 @@
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
@@ -15,12 +16,22 @@ page_content = driver.page_source
 webpage = BeautifulSoup(page_content, 'html.parser')
 events = webpage.find_all('div', class_='x78zum5 x1n2onr6 xh8yej3')
 
+event_list = []
+
 for event in events:
     event_image = event.find('img', class_='x1rg5ohu x5yr21d xl1xv1r xh8yej3')
-
     image_url = event_image['src'] if event_image and 'src' in event_image.attrs else None
 
-    print(event.text)
-    print(f"Image URL: {image_url}")
+    event_info = {
+        'Event': event.text,
+        'image_url': image_url
+    }
+
+    event_list.append(event_info)
 
 driver.quit()
+
+# Convert the list of dictionaries to a JSON string
+json_data = json.dumps(event_list, indent=2)
+
+print(json_data)
