962bc8d3ef078f82efd1c271e01fb2d0d5137126
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-18 14:44:23 -0300

    working on

--- ticketmaster.py
+++ ticketmaster.py
@@ -18,8 +18,10 @@ events = webpage.find_all('div', class_='sc-fyofxi-0 MDVIb')
 
 for event in events:
     event_name = event.find('span', class_='sc-fyofxi-5 gJmuwa').text
+    event_date_month = event.find('div', class_='sc-1evs0j0-1 gwWuEQ').text.strip()
 
     print(f"{event_name}")
+    print(f"{event_date_month}")
     # print(event.text)
 
 driver.quit()
