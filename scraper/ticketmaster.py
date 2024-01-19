93a513b2b279c49dc29e22e9fcf81329c7e2b008
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-19 09:39:14 -0300

    working on

--- scraper/ticketmaster.py
+++ scraper/ticketmaster.py
@@ -34,8 +34,11 @@ for event in events:
 
     event_list.append(event_info)
 
+
 driver.quit()
 
-json_data = json.dumps(event_list, indent=2)
+with open('events_data.json', 'w', encoding='utf-8') as json_file:
+    json.dump(event_list, json_file, ensure_ascii=False, indent=2)
+# json_data = json.dumps(event_list, indent=2)
 
-print(json_data)
+# print(json_data)
