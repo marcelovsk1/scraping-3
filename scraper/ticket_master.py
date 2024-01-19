1b625abb128c1b5b3653c1f37db49161c004d34b
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-19 11:05:23 -0300

    working on

---
+++ scraper/ticket_master.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+url = 'https://www.ticketmaster.ca/discover/concerts/montreal'
+
+options = Options()
+options.headless = True
+
+driver = webdriver.Chrome(options=options)
+driver.get(url)
+driver.implicitly_wait(10)
+
+page_content = driver.page_source
+webpage = BeautifulSoup(page_content, 'html.parser')
+
+events = webpage.find_all('div', class_='Flex-sc-145abwg-0 bWTqsV accordion__item event-listing__item')
+
+for event in events:
+    event_name = event.find('div', class_='sc-fFeiMQ bCvzDL text text--dark text--primary sc-6jnhqk-0 kGOLzf event-tile__title').text
+    # event_date = event.find('div', class_='sc-1evs0j0-1 gwWuEQ').text.strip()
+#     event_day = event.find('div', class_='sc-1evs0j0-2 ftHsmv').text.strip()
+    event_location = event.find('div', class_='sc-fFeiMQ iIgzpz text text--dark text--secondary sc-1s3i3gy-0 hbRPym event-tile__sub-title').text.strip()
+
+    print(f"Event:{event_name}")
+    # print(f"{event_date}")
+#     print(f"{event_day}")
+    print(f"Location:{event_location}")
+
+driver.quit()
