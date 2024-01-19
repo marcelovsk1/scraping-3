dd4471abf58081a750b09dd76676dd12bb1c1d05
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-19 11:13:46 -0300

    working on

--- scraper/ticket_master.py
+++ scraper/ticket_master.py
@@ -18,13 +18,13 @@ events = webpage.find_all('div', class_='Flex-sc-145abwg-0 bWTqsV accordion__ite
 
 for event in events:
     event_name = event.find('div', class_='sc-fFeiMQ bCvzDL text text--dark text--primary sc-6jnhqk-0 kGOLzf event-tile__title').text
-    # event_date = event.find('div', class_='sc-1evs0j0-1 gwWuEQ').text.strip()
+    event_date = event.find('div', class_='sc-fFeiMQ dBYlim text text--accent text--accent01 text-tm sc-17ev1tv-0 cnj20n-0 firocR iZsGLV event-tile__date-title').text.strip()
 #     event_day = event.find('div', class_='sc-1evs0j0-2 ftHsmv').text.strip()
     event_location = event.find('div', class_='sc-fFeiMQ iIgzpz text text--dark text--secondary sc-1s3i3gy-0 hbRPym event-tile__sub-title').text.strip()
 
-    print(f"Event:{event_name}")
-    # print(f"{event_date}")
+    print(f"Event: {event_name}")
+    print(f"Date: {event_date}")
 #     print(f"{event_day}")
-    print(f"Location:{event_location}")
+    print(f"Location: {event_location}")
 
 driver.quit()
